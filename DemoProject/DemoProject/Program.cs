614e5f4815522a7eb46780f07ac2701fe6071c9e
Rikai23 <andrei.rihard.99@yandex.ru>
2023-07-07 01:02:14 +0300

    Добавили проект на C#

---
+++ DemoProject/DemoProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
